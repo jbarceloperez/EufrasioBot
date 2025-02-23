bbc95bdb1d14b2733299537799d06191ef4607d6
Javier Barceló <javibarpe@hotmail.com>
2025-02-24 00:40:25 +0100

    added test command

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
-const Discord = require('discord.js');
+const { Client, Events, SlashCommandBuilder } = require('discord.js');
 const { token } = require('./config.json');
-// const dotenv = require('dotenv');
-// const { REST } = require('@discordjs/rest');
-// const { Routes } = require('@discord-api-types/v9');
-// const fs = require('fs');
-// const { Player } = require('discord-player');
 
-const TOKEN = token;
-console.log(TOKEN);
-const client = new Discord.Client({ intents: [] });
-client.login(TOKEN);
+const client = new Client({ intents: [] });
+client.once(Events.ClientReady, c => {
+    console.log(`Logging in as ${c.user.username}`);
+    const ping = new SlashCommandBuilder()
+        .setName("ping")
+        .setDescription("Replies with 'pong'!");
+    client.application.commands.create(ping);
+});
+client.login(token);
+
+client.on(Events.InteractionCreate, interaction => {
+    // console.log(interaction);
+    if (interaction.commandName === "ping")
+        interaction.reply("como que pin ezo que mierda EEEH");
+});
